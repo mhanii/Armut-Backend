cd7238c599a605ceb084deebe46d7bfbf803fef3
Hani <mohamedhani590@gmail.com>
2025-07-05 04:20:37 +0300

    Add build script + updat requirements.txt

--- main/settings.py
+++ main/settings.py
@@ -103,11 +103,17 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 # Use PostgreSQL on Vercel if DATABASE_URL is available
